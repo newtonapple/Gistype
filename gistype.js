cf6b6d63f7b982305a36ac9949c07c52295e0480
David <david.github@gmail.com>
2012-04-23 23:55:33 -0700

    Adding stats: cpm, wpm & error rate.

--- gistype.js
+++ gistype.js
@@ -3,7 +3,7 @@ var Gistype = (function($) {
         gt.typed = function(selector, callback) {
             var input = $(selector);
             input.keydown(function(e) {
-                if (e.metaKey || e.altKey || e.ctrlKey) return;
+                if (e.metaKey || e.ctrlKey) return;
                 var keycode = KeyCode.translate_event(e);
                 if ( keycode.char ) {
                     input.trigger('typed.gistype', [keycode.char]);
@@ -20,44 +20,77 @@ var Gistype = (function($) {
         };
 
         gt.Display = function(selector) {
-            this.$el = $(selector);
-            this.el  = this.$el[0];
-            this.cursor = null;
+            this.$el        = $(selector);
+            this.el         = this.$el[0];
+            this.reset();
         };
         
         gt.Display.prototype = {
             setText: function(text) {
-                this.$el.text(text);
-                this.unmatched = this.el.firstChild;
-                this.moveCursor();
+                this.text = text;
+                this.reset();
             },
 
             match: function(key) {
-                // console.log('key: ' + key + ' cursor: ' + this.cursor.text())
-                if ( this.cursor && key !== this.cursor.text() ) {
-                    this.cursor.addClass('error');
+                if ( !this.done ) {
+                    if ( !this.firstMatchAt )
+                        this.firstMatchAt = new Date();
+                    if ( this.cursor && key !== this.cursor.text() ) {
+                        this.errorsCount++;
+                        this.cursor.addClass('error');
+                    }
+                    this.moveCursor();
+                    this.charsCount++;
                 }
-                this.moveCursor();
             },
-            
+
+            reset: function() {
+                this.charsCount  = 0;
+                this.errorsCount = 0;
+                this.firstMatchAt   = null;
+                this.cursor      = null;
+                this.done        = true;
+                if ( this.text ) {
+                    this.$el.text(this.text);
+                    this.done = false;
+                    this.unmatched = this.el.firstChild;
+                    this.moveCursor();
+                }
+            },
+
             moveCursor: function() {
                 this.cursor && this.cursor.removeClass('cursor');               
                 if ( this.unmatched && this.unmatched.data && this.unmatched.data.length > 0 ) {
-                        var parent = this.el,
-                            rest = this.unmatched.splitText(1),
-                            head = rest.previousSibling,
-                            headClone = head.cloneNode(true),
-                            cursor = document.createElement('span');
-                        cursor.className = 'cursor';
-                        cursor.appendChild(headClone);
-                        parent.replaceChild(cursor, head);
-                        this.unmatched = rest;
-                        this.cursor = $(cursor);
-                    return true;
+                    var parent = this.el,
+                        rest = this.unmatched.splitText(1),
+                        head = rest.previousSibling,
+                        headClone = head.cloneNode(true),
+                        cursor = document.createElement('span');
+                    cursor.className = 'cursor';
+                    cursor.appendChild(headClone);
+                    parent.replaceChild(cursor, head);
+                    this.unmatched = rest;
+                    this.cursor = $(cursor);
                 } else {
-                    return false;
+                    this.done = true;
                 }
+            },
+
+            errorRate: function() {
+                if ( this.charsCount === 0) return 0;
+                return this.errorsCount / this.charsCount;
+            },
+
+            wpm: function() {
+                return this.cpm() / 5;
+            },
+
+            cpm: function() {
+                if ( !this.firstMatchAt ) return 0;
+                var delta = (new Date() - this.firstMatchAt) / 60000;
+                return this.charsCount / delta;     
             }
+
         };
 
         return gt;
